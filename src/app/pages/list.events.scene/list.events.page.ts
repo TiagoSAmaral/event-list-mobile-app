e43fa1b183ff4133a3cab80d223bed1f7c031ef8
Tiago Amaral <tiagofly@outlook.com>
2025-09-10 04:06:14 -0300

    Update list.events.page.ts Implementacao de requisicao para exibir dados de eventos na lista.

--- src/app/pages/list.events.scene/list.events.page.ts
+++ src/app/pages/list.events.scene/list.events.page.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, Inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonRefresher, RefresherCustomEvent, IonRefresherContent } from '@ionic/angular/standalone';
 import pageIdentifier from '@shared/components/tabs/tabs.identifier';
+import { CardTitleDescriptionView } from '@shared/components/cards/card/card.title.description.view';
+import INetworkRequest, { NETWORK_REQUEST } from '@shared/interfaces/network.request.interface';
+import EventModel from '@shared/components/models/event.model';
+import ApiRequiriments from '@shared/services/network/api.requiriments';
+import IResponseDefault from '@shared/interfaces/response.default';
+
 
 @Component({
   selector: 'app-list.events',
   templateUrl: 'list.events.page.html',
   styleUrls: ['list.events.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonRefresherContent, IonRefresher, IonList, IonHeader, IonToolbar, IonTitle, IonContent, CardTitleDescriptionView],
 })
 export class ListEventsScenePage {
 
-  title = pageIdentifier["listEventPage"].title;;
-  constructor() {}
+  title = pageIdentifier["listEventPage"].title;
+  content: any[] = [];
+
+  constructor(@Inject(NETWORK_REQUEST) private networkRequest: INetworkRequest) {}
+
+  ionViewDidEnter() {
+    this.requestPageContent();
+  }
+
+  openDescription = (identifier: string | undefined) => {
+    console.log(identifier);
+  }
+
+  requestPageContent = (event: RefresherCustomEvent | undefined = undefined) => {
+    this.networkRequest.request<IResponseDefault<EventModel[]>>(ApiRequiriments.listEvent()).subscribe(response => {      
+
+      if (response.data == undefined)
+        return;
+
+      this.content = [];
+
+      response.data?.forEach( event => {
+          const humanDate = new Date(event.date);          
+          this.content.push({identifier: event.id,  title: event.title, description: humanDate.toLocaleDateString('pt-BR')});
+      });
+    });
+
+    event?.target.complete();
+  }
 }
