66a2143fe36de90fb218e051b1106f17a440f0e7
Tiago Amaral <tiagofly@outlook.com>
2025-09-09 23:40:44 -0300

    Update detail.event.page.html, detail.event.page.scss, detail.event.page.spec.ts, and 1 more file Implementacao base da da tela Detalhes de Eventos.

---
+++ src/app/pages/detail.event.page/detail.event.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import pageIdentifier from '@shared/components/tabs/tabs.identifier';
+
+@Component({
+  selector: 'app-detail.event.page',
+  templateUrl: 'detail.event.page.html',
+  styleUrls: ['detail.event.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class DetailEventPage {
+
+  title = pageIdentifier["eventDetailPage"].title;;
+  constructor() {}
+}
