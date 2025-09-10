dcc964fb2a716d97fa013292d0d0a12160e521b4
Tiago Amaral <tiagofly@outlook.com>
2025-09-09 23:39:22 -0300

    Update app.routes.ts, tabs.routes.ts, and main.ts Refatoracao de importacao de pages.

--- src/app/shared/components/tabs/tabs.routes.ts
+++ src/app/shared/components/tabs/tabs.routes.ts
@@ -8,18 +8,11 @@ export const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+        loadComponent: () => import('@pages/list.events.scene/list.events.page').then((m) => m.ListEventsScenePage),
       },
       {
         path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+        loadComponent: () => import('@pages/create.events.page/create.events.page').then((m) => m.CreateEventsPage),
       },
       {
         path: '',
